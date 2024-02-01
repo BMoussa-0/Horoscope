71efc61d3d1b062002f482146b20f93effe7689d
BatmanDuTiersMonde <157691509+BatmanDuTiersMonde@users.noreply.github.com>
2024-02-01 11:55:27 +0100

    Add files via upload

---
+++ horoscope/models/model.php
@@ -0,0 +1,18 @@
+            <?php
+            // Définition du tableau avec les signes astrologiques et les horoscopes
+            $signe = array(
+                "Bélier" => " Vous devez faire les efforts les plus serieux pour acquerir un bon niveau.",
+                "Taureau", "Une de vos plus grande qualité est de saisir les theories." ,
+                 "Gémeaux", "De temps a autre, admettez que vous n'etes pas le seul a raisonner avec logique." ,
+                 "Cancer", "La rapiditer de votre intelligence vous entrainera a negliger certain detail." ,
+                 "Lion","Un effort vers la patience et la perseverance sera pour vous grandement benefique" ,
+                 "Vierge","Faite l'effort de vous concentrer." ,
+                 "Balance", "Vous avez une forte tendance a choisir les solutions astucieuses." ,
+                 "Scorpion", "Renocer a imposer vos propres données et appliquer les methodes apprise." ,
+                 "Sagittaire", "Pardoner avec aisance et sans ambiguiter, mais n'acceptez aucun extreme" ,
+                 "Capricorne", "Une certaine faciliter ne doit pas vous detourner de vos de objectif." ,
+                 "Verseau", "Ce travail vous demandera beaucoup d'ingeniositer." ,
+                 "Poissons", "Votre exprit vif et limpide facilite la resolution des problemes." 
+             );
+            ?>
+            
---
+++ horoscope/views/choix.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horosocope</title>
+</head>
+<body>
+<p>
+<label for="signe">Choissisez votre signe
+</label><br />
+<select name="signe" id="signe">
+<option value="Bélier">Bélier</option>
+<option value="Taureau">Taureau</option>
+<option value="Gémeaux">Gémeaux</option>
+<option value="Cancer">Cancer</option>
+<option value="Lion">Lion</option>
+<option value="Vierge">Vierge</option>
+<option value="Balance">Balance</option>
+<option value="Scorpion">Scorpion</option>
+<option value="Sagittaire">Sagittaire</option>
+<option value="Capricorne">Capricorne</option>
+<option value="Verseau">Verseau</option>
+<option value="Poisson">Poisson</option>
+<input type="submit" value="OK">
+</select>
+</p>
+</body>
+</html>
+
